b49825420df55e869c84c379d82f2a24998af523
edisoncardenas151999 <108204103+edisoncardenas151999@users.noreply.github.com>
2023-08-25 17:30:15 -0400

    first commit

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -15,7 +15,7 @@ const Sidenav = () => {
         className="absolute top-4 right-4 z-[99] md:hidden"
       />
       {nav ? (
-        <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center z-20">
+        <div className="fixed w-full pl-14 h-screen bg-white/90 flex flex-col justify-center z-20">
           <a
             onClick={handleNav}
             href="#main"
@@ -30,7 +30,7 @@ const Sidenav = () => {
             className="w-[75%] flex justify-center items-center rounded-full shawdow-lg bg-gray-100 shadow gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
-            <span className="pl-4">Projects</span>
+            <span className="pl-4">Project</span>
           </a>
           <a
             onClick={handleNav}
