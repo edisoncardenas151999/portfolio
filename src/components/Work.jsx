0fa6bcd3af0756a463e4f7518af0d636a17e6a1b
edisoncardenas151999 <108204103+edisoncardenas151999@users.noreply.github.com>
2023-08-31 13:57:30 -0400

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,7 +22,7 @@ const Work = () => {
   return (
     <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="text-4xl pb-8 font-bold text-center text-[#001b5e] ">
-        Work Exprerience
+        Work Experience
       </h1>
       {data.map((item, idx) => (
         <WorkItem
